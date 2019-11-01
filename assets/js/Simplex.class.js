cdb7b8c8ce425b1c2585c77c52f572004c1ce034
Secco2112 <gustavomarini9@gmail.com>
2019-11-01 01:27:29 -0300

    Montando variáveis principais

--- assets/js/Simplex.class.js
+++ assets/js/Simplex.class.js
@@ -1,7 +1,66 @@
 class Simplex{
 
     constructor() {
-        
+        this.variables = null;
+        this.original_objective_function = null;
+        this.objective_function = null;
+        this.original_inequality_restrictions = null;
+        this.inequality_restrictions = null;
+        this.clearances = null;
+    }
+
+    setVariables(variables) {
+        this.variables = variables;
+        return this;
+    }
+
+    getVariables() {
+        return this.variables;
+    }
+
+    setOriginalObjectiveFunction(original_objective_function) {
+        this.original_objective_function = original_objective_function;
+        return this;
+    }
+
+    getOriginalObjectiveFunction() {
+        return this.original_objective_function;
+    }
+
+    setObjectiveFunction(objective_function) {
+        this.objective_function = objective_function;
+        return this;
+    }
+
+    getObjectiveFunction() {
+        return this.objective_function;
+    }
+
+    setOriginalInequalityRestrictions(original_inequality_restrictions) {
+        this.original_inequality_restrictions = original_inequality_restrictions;
+        return this;
+    }
+
+    getOriginalInequalityRestrictions() {
+        return this.original_inequality_restrictions;
+    }
+
+    setInequalityRestrictions(inequality_restrictions) {
+        this.inequality_restrictions = inequality_restrictions;
+        return this;
+    }
+
+    getInequalityRestrictions() {
+        return this.inequality_restrictions;
+    }
+
+    setClearances(clearances) {
+        this.clearances = clearances;
+        return this;
+    }
+
+    getClearances() {
+        return this.clearances;
     }
 
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
+var simplex = new Simplex();
+
 var IR_HTML = '<li class="inequality-restriction"><i class="fa fa-times delete-inequality-restriction"></i><div data-index="1" class="variable-group">    <input name="x1" type="text" class="form-control" />    <span> x1 </span></div><select class="form-control" name="operation">    <option value="plus">+</option>    <option value="subtract">-</option></select><div data-index="2" class="variable-group">    <input name="x2" type="text" class="form-control" />    <span> x2 </span></div><select class="form-control" name="equality">    <option value="greater-equal">>=</option>    <option value="smaller-equal"><=</option>    <option value="equal">=</option>    <option value="greater">></option>    <option value="smaller"><</option></select><input name="b" type="text" class="form-control" /></li>';
 var OPERATION_SELECT_HTML = '<select class="form-control" name="operation"><option value="plus">+</option><option value="subtract">-</option></select>';
-var EQUALITY_SELECT_HTML = '<select class="form-control" name="equality"><option value="greater-equal">>=</option><option value="smaller-equal"><=</option><option value="equal">=</option><option value="greater">></option><option value="smaller"><</option></select>';
+var EQUALITY_SELECT_HTML = '<select class="form-control" name="equality"><option value="smaller-equal"><=</option><option value="greater-equal">>=</option><option value="equal">=</option><option value="smaller"><</option><option value="greater">></option></select>';
 
 $(document).ready(function() {
     add_ir();
@@ -8,6 +10,8 @@ $(document).ready(function() {
 
     add_variable();
     delete_variable();
+
+    calculate();
 });
 
 
@@ -123,5 +127,87 @@ function delete_variable() {
         } else {
             alert("Não é possível ter menos de duas variáveis.");
         }
+    });
+}
+
+
+function calculate() {
+    $(".options .calculate").on("click", function() {
+        var self = this,
+            variables = [];
+        
+        $(".restrictions").find("> span").map(function(i, span) {
+            variables.push($(span).text().replace(">= 0", "").trim());
+        });
+
+        simplex.setVariables(variables);
+
+
+        // Função objetivo
+        var objective_function_el = $(".objective-function");
+        var original_objective_function = {};
+        var objective_function = {"Z": 1};
+
+        $(objective_function_el).find(".variable-group").each(function(i, vg) {
+            if($(vg).prev().attr("name") == "operation") {
+                var value = $(vg).find("input").val().trim();
+                var signal = $(vg).prev().val();
+                if(signal == "subtract") {
+                    value = (value[0] == "-"? value.replace("-", ""): "-" + value);
+                }
+                original_objective_function[variables[i]] = parseInt(value);
+                objective_function[variables[i]] = parseInt(value) * -1;
+            } else {
+                original_objective_function[variables[i]] = parseInt($(vg).find("input").val().trim());
+                objective_function[variables[i]] = parseInt($(vg).find("input").val().trim()) * -1;
+            }
+        });
+
+        simplex.setOriginalObjectiveFunction(original_objective_function);
+        simplex.setObjectiveFunction(objective_function);
+
+
+        // Restrições de inequação
+        var original_inequality_restrictions = [],
+            inequality_restrictions = [];
+
+        var folgas = [];
+        $(".list-inequality-restrictions li").each(function(i, li) {
+            folgas.push("xF" + (i + 1));
+        });
+
+        simplex.setClearances(folgas);
+
+        $(".list-inequality-restrictions li").each(function(i, li) {
+            var ir = {};
+            $(li).find(".variable-group").each(function(j, vg) {
+                if($(vg).prev().attr("name") == "operation") {
+                    var value = $(vg).find("input").val().trim();
+                    var signal = $(vg).prev().val();
+                    if(signal == "subtract") {
+                        value = (value[0] == "-"? value.replace("-", ""): "-" + value);
+                    }
+                    ir[variables[j]] = parseInt(value);
+                } else {
+                    ir[variables[j]] = parseInt($(vg).find("input").val().trim());
+                }
+            });
+
+            ir["b"] = parseInt($(li).find("input[name=b]").val().trim());
+
+            original_inequality_restrictions.push(ir);
+
+            // Adiciona folgas
+            var tmp = Object.assign({}, ir);
+            $.each(folgas, function(j, f) {
+                tmp["xF" + (j + 1)] = (+(j == i));
+            });
+            inequality_restrictions.push(tmp);
+        });
+
+        simplex.setOriginalInequalityRestrictions(original_inequality_restrictions);
+        simplex.setInequalityRestrictions(inequality_restrictions);
+
+        console.log(simplex);
     });
 }
